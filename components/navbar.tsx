b96c62f3724e5f2e98dcf710ea2672d238bb8742
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-04-28 22:21:16 +0530

    forms added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -47,14 +47,14 @@ const Navbar = () => {
                 Home
               </Link>
               <Link
-                href="/students"
+                href="/forms/studentLogin"
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
                 Student
               </Link>
               <Link
-                href="/admin"
+                href="/forms/adminLogin"
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
